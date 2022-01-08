edb2e4668b9a7a32954f4683b6eb2f71987c238a
RAHUL BAIRAGI <73958355+rahulcode751@users.noreply.github.com>
2022-01-08 15:23:42 +0530

    Update email_using_python.py

--- Send_email_using_python/email_using_python.py
+++ Send_email_using_python/email_using_python.py
@@ -1,6 +1,6 @@
 import smtplib # if you want to send email we use smtp simple mail transfer protocol
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.starttls() # coonection by server.start and fro security we use starttls i.e transpose layer security
-server.login('rahulbairagiofficial@gmail.com','201728627')
+server.login('rahulbairagiofficial@gmail.com','2017****')
 server.sendmail('rahulbairagiofficial@gmail.com','aditibairagiofficial@gamil.com','Mail send from rahulcode751')
 print("Mail sent")
